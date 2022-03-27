a015c24ec9f1e87f15de2040266f7eea8d03ed07
Steven Wu <swu121@gmail.com>
2022-03-26 21:10:57 -0700

    Added Styling for the login and register page

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,13 +1,51 @@
 <template>
-    <div class = "login">
-        <h1>Login</h1>
-        <form @submit.prevent="Login">
-            <input type="text" placeholder = "Email Address" v-model = 'email'/>
-            <input type="password" placeholder = "Enter Password" v-model = 'password' />
-            <input type="submit" value="Login">
-            <p>Need an account? <router-link to="/register">Register Here</router-link></p>
-        </form>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <section class="vh-100" style="background-color: #black;">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col col-xl-10">
+            <div class="card" style="border-radius: 1rem;">
+            <div class="row g-0">
+                <div class="col-md-6 col-lg-5 d-none d-md-block">
+                <img
+                    src="../assets/Holiday_2019_loading_screen.jpg"
+                    alt="login form"
+                    class="img-fluid" style="border-radius: 1rem 0 0 1rem;"
+                />
+                </div>
+                <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                <div class="card-body p-4 p-lg-5 text-black">
+
+                    <form>
+                    <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
+
+                    <div class="form-outline mb-4">
+                        <input type="email" id="form2Example17" class="form-control form-control-lg" v-model = 'email' />
+                        <label class="form-label" for="form2Example17">Email address</label>
+                    </div>
+
+                    <div class="form-outline mb-4">
+                        <input type="password" id="form2Example27" class="form-control form-control-lg" v-model = 'password'/>
+                        <label class="form-label" for="form2Example27">Password</label>
+                    </div>
+
+                    <div class="pt-1 mb-4">
+                        <button class="btn btn-dark btn-lg btn-block" type="button" value = 'Login'>Login</button>
+                    </div>
+
+                    <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <router-link to="/register">Register Here</router-link></p>
+                    <a href="#!" class="small text-muted">Terms of use.</a>
+                    <a href="#!" class="small text-muted">Privacy policy</a>
+                    </form>
+
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
     </div>
+    </section>
 </template>
 
 <script lang="ts">
